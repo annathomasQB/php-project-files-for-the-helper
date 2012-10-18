8dff4a887b8843be3ae7b2a2e7dad4d2cd534ca3
Anna Thomas <annat@qburst.com>
2012-10-18 09:09:15 +0530

    working condition - need to check on derived float value

--- tipServerSideCalculation.php
+++ tipServerSideCalculation.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body>
         <?php
         $bill = mysql_real_escape_string($_GET['billAmount']);
         $rate = mysql_real_escape_string($_GET['tipRate']);
@@ -14,5 +7,3 @@
         $json = array('TipCalculator'=>$arr);
         echo json_encode($json);
         ?>
-    </body>
-</html>
